76342a730015dfe2c6c2a64af20c4ec2f957bba6
Maharshi Banerjee <116052111+mbanerjee22@users.noreply.github.com>
2023-05-16 20:53:35 +0530

    Update hello.js

--- src/hello.js
+++ src/hello.js
@@ -5,7 +5,7 @@ const hello = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "implemented serverless with aws & cicd",
+        message: "implemented serverless with aws & cicd monitoring",
       },
       null,
       2
